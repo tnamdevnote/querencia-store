48354cad2370ec9d1fc4501fe7be7399609f2eeb
Taek Been Nam <taekbeen93@gmail.com>
2024-01-21 02:42:15 -0500

    style: setup theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "primary-dark": "#2A254B",
+        primary: "#4E4D93",
+        "light-gray": "#F9F9F9",
+        "border-gray": "#EBE8F4",
+        "border-dark": "#CAC6DA",
+      },
+      fontFamily: {
+        headings: ["Montserrat", "sans"],
+        body: ["DM Sans", , "sans"],
+      },
+      fontSize: {
+        sm: ["0.875rem", "150%"],
+        md: ["1rem", "150%"],
+        lg: ["1.25rem", "150%"],
+        "3xl": ["2rem", "150%"],
+      },
+    },
   },
   plugins: [],
 };
