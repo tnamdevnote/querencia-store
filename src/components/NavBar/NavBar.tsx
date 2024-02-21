9c742330c0a1e53573b6227c744ed7064efeca94
Taek Been Nam <taekbeen93@gmail.com>
2024-02-21 14:02:37 -0500

    responsive navbar

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom";
 import Icon from "../Icons/Icon";
+import { useState } from "react";
 
 const categories = [
   "All Products",
@@ -11,6 +12,11 @@ const categories = [
 ];
 
 function NavBar() {
+  const [isOpen, setOpen] = useState(false);
+
+  const handleMenuOpen = () => {
+    setOpen(!isOpen);
+  };
   return (
     <header>
       <div className="flex items-center justify-between p-5">
@@ -24,13 +30,29 @@ function NavBar() {
           <div>
             <Icon name="Avatar" className="h-4 w-4" />
           </div>
-          <button>
+          <button className="md:hidden" onClick={handleMenuOpen}>
             <Icon name="Menu" className="h-4 w-4" />
           </button>
         </div>
       </div>
       <nav className="relative">
-        <ul className="absolute left-1/2 flex w-full -translate-x-1/2 flex-col items-center gap-2 bg-white/10 backdrop-blur-sm">
+        <div
+          className={`min-h-screen w-full bg-white/10 backdrop-blur-sm ${isOpen ? "block" : "hidden"} md:hidden`}
+        >
+          <ul
+            className={`absolute left-1/2 ${isOpen ? "flex" : "hidden"} w-full -translate-x-1/2 flex-col items-center gap-2 py-5`}
+          >
+            {categories.map((category, i) => (
+              <li key={i} className="font-body text-md text-primary">
+                <a href="#">{category}</a>
+              </li>
+            ))}
+          </ul>
+        </div>
+        <hr></hr>
+        <ul
+          className={`hidden w-full gap-2 py-5 md:flex md:justify-center md:gap-11`}
+        >
           {categories.map((category, i) => (
             <li key={i} className="font-body text-md text-primary">
               <a href="#">{category}</a>
