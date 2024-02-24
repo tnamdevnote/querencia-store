4d5345a956cdb5889237c7f87f8674fbebab3ad7
Taek Been Nam <taekbeen93@gmail.com>
2024-02-24 18:43:50 -0500

    update tailwind config for custom theme

--- src/components/Button/Button.stories.tsx
+++ src/components/Button/Button.stories.tsx
@@ -4,13 +4,14 @@ import Button from "./Button";
 const meta: Meta<typeof Button> = {
   title: "Components/Button",
   component: Button,
+  args: {
+    label: "Button",
+  },
 };
 
 export default meta;
 type Story = StoryObj<typeof Button>;
 
-export const Default: Story = {
-  args: {
-    children: "Button",
-  },
-};
+export const Primary: Story = {};
+export const White: Story = {};
+export const Ghost: Story = {};
--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,14 +1,42 @@
 import { forwardRef } from "react";
 
-const Button = forwardRef<
-  HTMLButtonElement,
-  React.ComponentPropsWithoutRef<"button">
->(({ children, ...props }, ref) => {
-  return (
-    <button type="button" ref={ref} {...props}>
-      {children}
-    </button>
-  );
-});
+export interface ButtonProps extends React.ComponentPropsWithoutRef<"button"> {
+  label?: string;
+  variant?:
+    | "primary"
+    | "secondary"
+    | "opaque"
+    | "white"
+    | "success"
+    | "error"
+    | "warning";
+  size: "small" | "medium" | "large";
+  isFullWidth?: boolean;
+}
+
+const Button = forwardRef<HTMLButtonElement, ButtonProps>(
+  (
+    {
+      children,
+      label,
+      variant = "primary",
+      size = "medium",
+      isFullWidth,
+      ...props
+    },
+    ref,
+  ) => {
+    return (
+      <button
+        type="button"
+        ref={ref}
+        {...props}
+        className="bg-primary-100 font-heading h-14 w-[114px] px-8 py-4"
+      >
+        {label ? label : children}
+      </button>
+    );
+  },
+);
 
 export default Button;
